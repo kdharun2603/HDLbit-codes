5d11684d0f9930da1968af15131acde356009ae2
kdharun2603 <146450968+kdharun2603@users.noreply.github.com>
2026-04-28 12:33:34 +0530

    another gate.v

---
+++ combinational logic/basic gates/another gate.v
@@ -0,0 +1,13 @@
+// Implement the following circuit:
+
+
+module top_module (
+    input in1,
+    input in2,
+    output out);
+    always @(*)begin
+        
+        out = {in1 & ~in2};
+    end
+    
+endmodule

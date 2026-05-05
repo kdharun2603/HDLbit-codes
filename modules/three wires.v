85e8bd361c3d0fd98b4cd9428fd9fb6f9440434a
kdharun2603 <kdharun2603@gmail.com>
2026-05-05 15:34:40 +0530

    three wires.v

---
+++ modules/three wires.v
@@ -0,0 +1,12 @@
+// You are given a module my_dff with two inputs and one output (that implements a D flip-flop). Instantiate three of them, then chain them together to make a shift register of length 3. The clk port needs to be connected to all instances.
+
+
+module top_module ( input clk, input d, output q );
+
+wire q0, q1;
+
+my_dff my_dff_0 (clk, d, q0);
+my_dff my_dff_1 (clk, q0, q1);
+my_dff my_dff_2 (clk, q1, q);
+
+endmodule
